61db3c940294a76a9af008f664309efcdfb2bdd6
chamcham0707 <cheamin6306@naver.com>
2024-06-17 13:41:12 +0900

    test DB 분리 - in memory H2로 테스트용 DB 분리. NewsfeedServiceTest에 적용

--- src/test/java/com/sparta/oneandzerobest/newsfeed/service/NewsfeedServiceIntegrationTest.java
+++ src/test/java/com/sparta/oneandzerobest/newsfeed/service/NewsfeedServiceIntegrationTest.java
@@ -3,10 +3,13 @@ package com.sparta.oneandzerobest.newsfeed.service;
 import com.sparta.oneandzerobest.auth.entity.User;
 import com.sparta.oneandzerobest.auth.entity.UserStatus;
 import com.sparta.oneandzerobest.auth.repository.UserRepository;
+import com.sparta.oneandzerobest.comment.dto.CommentRequestDto;
+import com.sparta.oneandzerobest.comment.entity.Comment;
 import com.sparta.oneandzerobest.newsfeed.dto.NewsfeedRequestDto;
 import com.sparta.oneandzerobest.newsfeed.dto.NewsfeedResponseDto;
 import com.sparta.oneandzerobest.newsfeed.entity.Newsfeed;
 import com.sparta.oneandzerobest.newsfeed.repository.NewsfeedRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +20,8 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ActiveProfiles;
+
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
@@ -28,32 +33,41 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NewsfeedServiceIntegrationTest {
 
     @Autowired
     private NewsfeedService newsfeedService;
 
-    @MockBean
+    @Autowired
     private UserRepository userRepository;
 
-    @MockBean
+    @Autowired
     private NewsfeedRepository newsfeedRepository;
+
+    User user;
+    Newsfeed newsfeed;
+    Newsfeed newsfeed2;
+
+    @BeforeEach
+    void setUp() {
+        user = new User("testUsername", "testPassword!", "testName", "testEmail@example.com", UserStatus.ACTIVE);
+        userRepository.save(user);
+
+        newsfeed = new Newsfeed(user.getId(), "this is newsfeed1 test");
+        newsfeed2 = new Newsfeed(user.getId(), "this is newsfeed2 test");
+        newsfeedRepository.save(newsfeed);
+        newsfeedRepository.save(newsfeed2);
+    }
 
     @Test
     @DisplayName("게시글 작성 테스트")
     void test1() {
         // Given
-        String username = "testUsername";
-        String content = "this is test";
-        Long userId = 1L;
-        NewsfeedRequestDto newsfeedRequestDto = new NewsfeedRequestDto(content);
-        User user = new User(userId, username, "testPassword!", "testName", "test@example.com", "test!!!", UserStatus.ACTIVE, "testRefreshToken", LocalDateTime.now(), LocalDateTime.now(), null);
-
-        when(userRepository.findByUsername(username)).thenReturn(Optional.of(user));
-        when(newsfeedRepository.save(any(Newsfeed.class))).thenReturn(new Newsfeed(userId, content));
+        NewsfeedRequestDto newsfeedRequestDto = new NewsfeedRequestDto("this is add test");
 
         // When
-        NewsfeedResponseDto newsfeedResponseDto = newsfeedService.postContent(username, newsfeedRequestDto).getBody();
+        NewsfeedResponseDto newsfeedResponseDto = newsfeedService.postContent(user.getUsername(), newsfeedRequestDto).getBody();
 
         // Then
         assertNotNull(newsfeedResponseDto);
@@ -64,77 +78,36 @@ class NewsfeedServiceIntegrationTest {
     @Test
     @DisplayName("게시글 조회 테스트")
     void test2() {
-        // Given
-        int page = 0;
-        int size = 10;
-        boolean isASC = true;
-        boolean like = false;
-        LocalDateTime startTime = null;
-        LocalDateTime endTime = null;
-
-        List<Newsfeed> newsfeeds = Arrays.asList(
-                new Newsfeed(1L, "Content1"),
-                new Newsfeed(2L, "Content2")
-        );
-
-        Page<Newsfeed> newsfeedPage = new PageImpl<>(newsfeeds);
-        when(newsfeedRepository.findAll(any(Pageable.class))).thenReturn(newsfeedPage);
-
         // When
         ResponseEntity<Page<NewsfeedResponseDto>> response = newsfeedService.getAllContents(
-                page, size, isASC, like, startTime, endTime);
+                0, 10, true, false, null, null);
 
         // Then
         assertThat(response.getStatusCodeValue()).isEqualTo(200);
         assertThat(response.getBody()).isNotNull();
-        assertThat(response.getBody().getTotalElements()).isEqualTo(newsfeeds.size());
-        verify(newsfeedRepository, times(1)).findAll(any(Pageable.class));
+        assertThat(response.getBody().getTotalElements()).isEqualTo(2);
     }
 
     @Test
     @DisplayName("게시물 수정 테스트")
     void test3() {
         // Given
-        String username = "testUsername";
-        Long contentId = 1L;
-        String content = "this is test";
-        String updateContent = "update test success";
-        NewsfeedRequestDto newsfeedRequestDto = new NewsfeedRequestDto(updateContent);
-        User user = new User(1L, username, "testPassword!", "testName", "test@example.com", "test!!", UserStatus.ACTIVE, "testRefreshToken", LocalDateTime.now(), LocalDateTime.now(), null);
-        Newsfeed newsfeed = new Newsfeed(user.getId(), content);
-
-        when(newsfeedRepository.findById(contentId)).thenReturn(Optional.of(newsfeed));
-        when(userRepository.findByUsername(username)).thenReturn(Optional.of(user));
+        NewsfeedRequestDto newsfeedRequestDto = new NewsfeedRequestDto("newsfeed update test");
 
         // When
-        newsfeedService.putContent(username, contentId, newsfeedRequestDto);
+        newsfeedService.putContent(user.getUsername(), newsfeed.getId(), newsfeedRequestDto);
 
         // Then
-        assertEquals(updateContent, newsfeed.getContent());
-
-        verify(userRepository, times(1)).findByUsername(username);
-        verify(newsfeedRepository, times(1)).findById(contentId);
+        assertEquals(newsfeedRequestDto.getContent(), newsfeed.getContent());
     }
 
     @Test
     @DisplayName("게시물 삭제 테스트")
     void test4() {
-        // Given
-        String username = "testUsername";
-        Long contentId = 1L;
-        User user = new User(1L, "testUsername", "testPassword1", "testName", "test@example.com", "test!!!", UserStatus.ACTIVE, "testRefreshToken", LocalDateTime.now(), LocalDateTime.now(), null);
-        Newsfeed newsfeed = new Newsfeed(user.getId(), "this is test");
-
-        when(newsfeedRepository.findById(contentId)).thenReturn(Optional.of(newsfeed));
-        when(userRepository.findByUsername(username)).thenReturn(Optional.of(user));
-
         // When
-        ResponseEntity<Long> result = newsfeedService.deleteContent(username, contentId);
+        ResponseEntity<Long> result = newsfeedService.deleteContent(user.getUsername(), newsfeed.getId());
 
         // Then
         assertEquals(newsfeed.getId(), result.getBody());
-
-        verify(newsfeedRepository, times(1)).findById(contentId);
-        verify(userRepository, times(1)).findByUsername(username);
     }
 }
